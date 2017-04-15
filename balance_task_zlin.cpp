c20b01213797762c44e330772925cfd342b41d65
zlin3 <tslzf@hotmail.com>
2017-04-14 17:37:47 -0700

    walk in space in balance_task_zlin.cpp

--- balance_task_zlin.cpp
+++ balance_task_zlin.cpp
@@ -29,6 +29,9 @@ Remarks:
 // local variables
 static double      start_time = 0.0;
 static SL_DJstate  target[N_DOFS+1];
+static SL_DJstate  target_right[N_DOFS+1];
+static SL_DJstate  target_left[N_DOFS+1];
+static SL_DJstate  target_base[N_DOFS+1];
 static SL_Cstate   cog_target;
 static SL_Cstate   cog_traj;
 static SL_Cstate   cog_ref;
@@ -36,13 +39,18 @@ static double      delta_t = 0.01;
 static double      duration = 10.0;
 static double      time_to_go;
 static int         which_step;
+static int         right_foot;
 
 // possible states of a state machine
 enum Steps {
   ASSIGN_COG_TARGET,
   MOVE_TO_COG_TARGET,
+  ASSIGN_COG_TARGET_BASE,
+  MOVE_TO_COG_TARGET_BASE,
   ASSIGN_JOINT_TARGET_LIFT_UP,
-  MOVE_JOINT_TARGET_LIFT_UP
+  MOVE_JOINT_TARGET_LIFT_UP,
+  MOVE_JOINT_TARGET_LEFT_DOWN,
+  ASSIGN_JOINT_TARGET_LEFT_DOWN
 };
 
 // variables for COG control
@@ -145,14 +153,21 @@ init_balance_task(void)
 
   // prepare going to the default posture
   bzero((char *)&(target[1]),N_DOFS*sizeof(target[1]));
-  for (i=1; i<=N_DOFS; i++)
+  bzero((char *)&(target_right[1]),N_DOFS*sizeof(target_right[1]));
+  bzero((char *)&(target_left[1]),N_DOFS*sizeof(target_left[1]));
+  bzero((char *)&(target_base[1]),N_DOFS*sizeof(target_base[1]));
+  for (i=1; i<=N_DOFS; i++) {
     target[i] = joint_default_state[i];
+    target_base[i] = joint_default_state[i];
+  }
 
+  right_foot = 1;
   // go to the target using inverse dynamics (ID)
   if (!go_target_wait_ID(target)) 
     return FALSE;
 
   // ready to go
+  freezeBase(FALSE);
   ans = 999;
   while (ans == 999) {
     if (!get_int("Enter 1 to start or anthing else to abort ...",ans,&ans))
@@ -217,9 +232,26 @@ run_balance_task(void)
 
     // what is the target for the COG?
     bzero((void *)&cog_target,sizeof(cog_target));
+#if 0
     cog_target.x[_X_] = cart_des_state[RIGHT_FOOT].x[_X_];
     cog_target.x[_Y_] = cart_des_state[RIGHT_FOOT].x[_Y_];
     cog_target.x[_Z_] = cart_des_state[RIGHT_FOOT].x[_Z_];
+# else
+    if (right_foot == 1) {
+//    cog_target.x[_X_] = (right_foot == 1) ? 0.054 : -0.054;
+//    cog_target.x[_Y_] = 0.012;
+//    cog_target.x[_Z_] = -0.119;
+    cog_target.x[_X_] = cart_des_state[RIGHT_FOOT].x[_X_];
+    cog_target.x[_Y_] = cart_des_state[RIGHT_FOOT].x[_Y_];
+    cog_target.x[_Z_] = cart_des_state[RIGHT_FOOT].x[_Z_];
+
+    } else {
+    cog_target.x[_X_] = cart_des_state[LEFT_FOOT].x[_X_];
+    cog_target.x[_Y_] = cart_des_state[LEFT_FOOT].x[_Y_];
+    cog_target.x[_Z_] = cart_des_state[LEFT_FOOT].x[_Z_];
+   
+    }
+#endif
     //cog_target.x[_Z_] = 0;
 
     // the structure cog_des has the current position of the COG computed from the
@@ -271,21 +303,6 @@ run_balance_task(void)
       joint_des_state[i].thdd = 0;
       joint_des_state[i].uff  = 0;
     }
-    // this is an indicator which Cartesian components of the endeffectors are constraints
-    // i.e., both feet are on the ground and cannot move in position or orientation
-    stat[RIGHT_FOOT][1] = TRUE;
-    stat[RIGHT_FOOT][2] = TRUE;
-    stat[RIGHT_FOOT][3] = TRUE;
-    stat[RIGHT_FOOT][4] = TRUE;
-    stat[RIGHT_FOOT][5] = TRUE;
-    stat[RIGHT_FOOT][6] = TRUE;
-
-    stat[LEFT_FOOT][1] = TRUE;
-    stat[LEFT_FOOT][2] = TRUE;
-    stat[LEFT_FOOT][3] = TRUE;
-    stat[LEFT_FOOT][4] = TRUE;
-    stat[LEFT_FOOT][5] = TRUE;
-    stat[LEFT_FOOT][6] = TRUE;
 
     // this is a special inverse dynamics computation for a free standing robot
     inverseDynamicsFloat(delta_t, stat, TRUE, joint_des_state, NULL, NULL, fc);
@@ -297,18 +314,112 @@ run_balance_task(void)
 
     break;
 
+  case ASSIGN_COG_TARGET_BASE:
+
+    // what is the target for the COG?
+    bzero((void *)&cog_target,sizeof(cog_target));
+#if 0
+    cog_target.x[_X_] = cart_des_state[RIGHT_FOOT].x[_X_];
+    cog_target.x[_Y_] = cart_des_state[RIGHT_FOOT].x[_Y_];
+    cog_target.x[_Z_] = cart_des_state[RIGHT_FOOT].x[_Z_];
+# else
+    cog_target.x[_X_] = 0.5 * (cart_des_state[LEFT_FOOT].x[_X_] + cart_des_state[RIGHT_FOOT].x[_X_]);
+    cog_target.x[_Y_] = 0.012;
+    cog_target.x[_Z_] = cart_des_state[RIGHT_FOOT].x[_Z_];
+#endif
+    //cog_target.x[_Z_] = 0;
+
+    // the structure cog_des has the current position of the COG computed from the
+    // joint_des_state of the robot. cog_des should track cog_traj
+    bzero((void *)&cog_traj,sizeof(cog_traj));
+    for (i=1; i<=N_CART; ++i)
+      cog_traj.x[i] = cog_des.x[i];
+
+    // time to go
+    time_to_go = duration;
+
+    // switch to next step of state machine
+    which_step = MOVE_TO_COG_TARGET_BASE;
+
+    break;
+
+  case MOVE_TO_COG_TARGET_BASE: // this is for inverse kinematics control
+
+    // plan the next step of cog with min jerk
+    for (i=1; i<=N_CART; ++i) {
+      min_jerk_next_step(cog_traj.x[i],
+			 cog_traj.xd[i],
+			 cog_traj.xdd[i],
+			 cog_target.x[i],
+			 cog_target.xd[i],
+			 cog_target.xdd[i],
+			 time_to_go,
+			 delta_t,
+			 &(cog_traj.x[i]),
+			 &(cog_traj.xd[i]),
+			 &(cog_traj.xdd[i]));
+    }
+
+    // inverse kinematics: we use a P controller to correct for tracking erros
+    for (i=1; i<=N_CART; ++i) {
+      cog_ref.xd[i] = kp*(cog_traj.x[i] - cog_des.x[i]) + cog_traj.xd[i];
+      // create a separate velocity vector for use of the provided mat_vec_mult routine 
+      delta_xd[i] = cog_ref.xd[i];
+    } 
+
+    // IK solution with constraint COG Jacobian
+    compute_cog_kinematics(stat, TRUE, FALSE, TRUE, Jccogp, NJccog);
+    mat_vec_mult(Jccogp, delta_xd, delta_thd);
+    // compute the joint_des_state[i].th and joint_des_state[i].thd  
+    for (i=1; i<=N_DOFS; ++i) {
+      // assign theta and theta_d accordingly
+      joint_des_state[i].thd  = delta_thd[i];
+      joint_des_state[i].th  += delta_thd[i] * delta_t;
+      joint_des_state[i].thdd = 0;
+      joint_des_state[i].uff  = 0;
+    }
+
+    // this is a special inverse dynamics computation for a free standing robot
+    inverseDynamicsFloat(delta_t, stat, TRUE, joint_des_state, NULL, NULL, fc);
+    // decrement time to go
+    time_to_go -= delta_t;
+    if (time_to_go <= 0) {
+      which_step = ASSIGN_COG_TARGET;
+    }
+
+    break;
+
+
   case ASSIGN_JOINT_TARGET_LIFT_UP:
 
     // initialize the target structure from the joint_des_state
-    for (i=1; i<=N_DOFS; ++i)
+    for (i=1; i<=N_DOFS; ++i) {
       target[i] = joint_des_state[i];
+      target_right[i] = joint_des_state[i];
+      target_left[i] = joint_des_state[i];
+    }
+#if 1
+    if (right_foot == 1) {
+      target[L_HAA].th -=  0.4;
+      target[L_AAA].th  =  0.5;
+      target[R_HAA].th -= 0.25;
+      target[R_AAA].th += 0.05;
+      right_foot = 0;
+    } else {
+      target[R_HAA].th -=  0.35;
+      target[R_AAA].th  = -0.4;
+      target[L_HAA].th -= 0.2;
+      target[L_AAA].th -= 0.03;
+      right_foot = 1;
+    }
 
+#else
     target[L_HFE].th =  0.68;
     target[L_HAA].th = -0.1;
     target[L_KFE].th =  0.95;
     target[L_AFE].th =  0.28;
     target[L_AAA].th =  0.20;
-
+#endif
     time_to_go = duration;  // this may be too fast -- maybe a slower movement is better
 
     which_step = MOVE_JOINT_TARGET_LIFT_UP;
@@ -332,13 +443,56 @@ run_balance_task(void)
 			 &(joint_des_state[i].thdd));
     }
 
-    SL_InvDynNE(joint_state,joint_des_state,endeff,&base_state,&base_orient);
+    //SL_InvDynNE(joint_state,joint_des_state,endeff,&base_state,&base_orient);
     // decrement time to go
     time_to_go -= delta_t;
 
     if (time_to_go <= 0) {
-      freeze();
+      which_step = ASSIGN_JOINT_TARGET_LEFT_DOWN;
+    }
 
+    break;
+
+  case ASSIGN_JOINT_TARGET_LEFT_DOWN:
+
+    // initialize the target structure from the joint_des_state
+    for (i=1; i<=N_DOFS; ++i) {
+      if (right_foot == 0) {
+        target[i] = target_right[i];
+      } else {
+        target[i] = target_left[i];
+      }
+    }
+
+    time_to_go = duration;  // this may be too fast -- maybe a slower movement is better
+
+    which_step = MOVE_JOINT_TARGET_LEFT_DOWN;
+
+    break;
+
+  case MOVE_JOINT_TARGET_LEFT_DOWN:
+
+    // compute the update for the desired states
+    for (i=1; i<=N_DOFS; ++i) {
+      min_jerk_next_step(joint_des_state[i].th,
+			 joint_des_state[i].thd,
+			 joint_des_state[i].thdd,
+			 target[i].th,
+			 target[i].thd,
+			 target[i].thdd,
+			 time_to_go,
+			 delta_t,
+			 &(joint_des_state[i].th),
+			 &(joint_des_state[i].thd),
+			 &(joint_des_state[i].thdd));
+    }
+
+    //SL_InvDynNE(joint_state,joint_des_state,endeff,&base_state,&base_orient);
+    // decrement time to go
+    time_to_go -= delta_t;
+
+    if (time_to_go <= 0) {
+      which_step = ASSIGN_COG_TARGET_BASE;
     }
 
     break;
